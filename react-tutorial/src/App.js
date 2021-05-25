c7e5b481c5f441782abfb1ae654d7031d5c28ed9
Lars Brevén <lars.breven@telia.com>
2021-05-25 21:18:54 +0200

    Section "State" finished.

--- react-tutorial/src/App.js
+++ react-tutorial/src/App.js
@@ -1,10 +1,11 @@
+import { render } from '@testing-library/react'
 import React, {Component} from 'react'
 import Table from './Table'
 
 class App extends Component {
 
-    render() {
-        const characters = [
+    state = {
+        characters : [
             {
                 name: 'Kalle',
                 job: 'Managing director',
@@ -21,13 +22,27 @@ class App extends Component {
                 name: 'Lasse',
                 job: 'Coffee machine cleaner',
             },
-        ]
+        ],
+    }
+
+    removeCharacter = (index) => {
+        const {characters}  = this.state;
+
+        this.setState({
+            characters: characters.filter((character, i) => {
+                return i !== index;
+            })
+        });
+    }
+
+    render() {
+        const {characters} = this.state;
 
         return (
             <div className="container">
-                <Table characterData={characters} />
+                <Table characterData={characters} removeCharacter={this.removeCharacter} />
             </div>
-        )
+        );
     }
 }
 
