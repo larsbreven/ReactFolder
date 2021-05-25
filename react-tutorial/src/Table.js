e4e4f4f043c5947b1531184edad6f68411251aef
Lars Brevén <lars.breven@telia.com>
2021-05-25 15:51:00 +0200

    Section "Simple components" finished.

--- react-tutorial/src/Table.js
+++ react-tutorial/src/Table.js
@@ -1,34 +1,46 @@
 import React, {Component} from 'react'
 
+const TableHeader = () => {
+    return (
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Job</th>
+            </tr>
+        </thead>
+    )
+}
+const TableBody = () => {
+    return (
+        <tbody>
+            <tr>
+                <td>Kalle</td>
+                <td>Managing director</td>
+            </tr>
+            <tr>
+                <td>Pelle</td>
+                <td>Administration manager</td>
+            </tr>
+            <tr>
+                <td>Olle</td>
+                <td>Beverage assistant</td>
+            </tr>
+            <tr>
+                <td>Lasse</td>
+                <td>Coffee machine cleaner</td>
+            </tr>
+        </tbody>
+      
+    )
+}
+
 class Table extends Component {
     render() {
         return (
             <table>
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Job</th>
-                    </tr>
-                </thead>
-                 <tbody>
-                <tr>
-                    <td>Kalle</td>
-                    <td>Managing director</td>
-                </tr>
-                <tr>
-                    <td>Pelle</td>
-                    <td>Administration manager</td>
-                </tr>
-                <tr>
-                    <td>Olle</td>
-                    <td>Beverage assistant</td>
-                </tr>
-                <tr>
-                    <td>Lasse</td>
-                    <td>Coffee machine cleaner</td>
-                </tr>
-            </tbody>
-        </table>
+                <TableHeader />
+                <TableBody/>
+            </table>
         )
     }
 }
